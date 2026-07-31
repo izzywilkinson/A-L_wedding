6a919a6d440f0a573136814652a997f6390e1971
izzywilkinson <izzywilky@outlook.com>
2026-07-31 19:43:53 +0100

    adding corner navigation to home

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+function toggleMenu() {
+
+    const menu = document.getElementById("menu");
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    }
+    else {
+        menu.style.display = "block";
+    }
+
+}
